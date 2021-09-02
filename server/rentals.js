b6f7e9bb8b27de2a9ba41dd0884881485d961c56
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-02 17:48:10 -0400

    update

--- server/rentals.js
+++ server/rentals.js
@@ -1,24 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const renter = require('../renters');
-  
+const renter = require("../renters");
 
-// POST 
-router.post('/renters', async (req, res) => {
-    const newRenter = await new Renter(
-      {
-        name: req.body.name,
-        weakness: req.body.weakness,
-        heroEnemy: req.body.heroEnemy,
-        alive: req.body.alive
-      }
-    ).save();
-  
-    try {
-      return res.json(newRenter)
-    } catch (error) {
-      return res.json({message: error})
-    }
-      
-  })
+// POST
+router.post("/renters", async (req, res) => {
+  const newRenter = await new Renter({
+    name: req.body.name,
+    book: req.body.book,
+    id: "",
+  }).save();
+
+  try {
+    return res.json(newRenter);
+  } catch (error) {
+    return res.json({ message: error });
+  }
+});
